65ef591580fed5dbad1e3b74337bb40466f6de6c
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 03:28:45 +0100

    Initial commit

---
+++ Shared.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Db = 'oxmysql' -- ghmattisql, oxmysql or mysql   NOTE: If you use last oxmysql just put mysql here.
+Config.Identifier = 'license' -- license, steam, discord
+Config.Admins = {
+    'license:17a4e3b2a5ccbca8290625cd1ab56852d1add411', -- Cookie
+    'license:91d7d64866bab92aac962264c6221a8e20c2fb1d', -- DX
+}
